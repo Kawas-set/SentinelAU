66e241e3abd1224291c664fa1e0156f2160ffbdc
Kawas-set <kiroshi504@gmail.com>
2026-04-14 03:44:05 +0300

    src/AssemblyInfo.cs

---
+++ AssemblyInfo.cs
@@ -0,0 +1,14 @@
+﻿using System.Diagnostics;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.Versioning;
+
+
+[assembly: AssemblyCompany("Kawasaki")]
+[assembly: AssemblyConfiguration("Debug")]
+[assembly: AssemblyDescription("SentinelAU")]
+[assembly: AssemblyFileVersion("1.1.1.1")]
+assembly: AssemblyInformationalVersion("1.1.1")]
+[assembly: AssemblyProduct("SentinelAU")]
+[assembly: AssemblyTitle("SentinelAU")]
+[assembly: AssemblyVersion("1.1.1.1")]
